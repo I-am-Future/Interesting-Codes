fe48424fa025e99c4579987836d96213a895e185
Lai Wei <74816944+I-am-Future@users.noreply.github.com>
2025-03-16 18:14:40 -0700

    Create starter8.sh

---
+++ Tmux-shortcuts/starter8.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# or zsh? #! /bin/zsh
+
+# Kill the session if it exists
+tmux kill-session -t proj
+
+# Create a new tmux session in the background
+tmux new-session -d -s proj
+
+# Split the window into 2 rows (upper and lower)
+tmux split-window -v -t proj:0.0
+
+# Split each row into 4 columns
+tmux split-window -h -t proj:0.0
+tmux split-window -h -t proj:0.1
+tmux split-window -h -t proj:0.0
+
+tmux split-window -h -t proj:0.4
+tmux split-window -h -t proj:0.5
+tmux split-window -h -t proj:0.4
+
+# Send echo messages to each panel
+tmux send-keys -t proj:0.0 'echo "Panel 1"; cd ~/.cache' C-m
+tmux send-keys -t proj:0.1 'echo "Panel 2"; cd ~/.cache' C-m
+tmux send-keys -t proj:0.2 'echo "Panel 3"; cd ~/.cache' C-m
+tmux send-keys -t proj:0.3 'echo "Panel 4"; cd ~/.cache' C-m
+tmux send-keys -t proj:0.4 'echo "Panel 5"; cd ~/.cache' C-m
+tmux send-keys -t proj:0.5 'echo "Panel 6"; cd ~/.cache' C-m
+tmux send-keys -t proj:0.6 'echo "Panel 7"; cd ~/.cache' C-m
+tmux send-keys -t proj:0.7 'echo "Panel 8"; cd ~/.cache' C-m
+
+# Attach to the session
+tmux attach -t proj
+
